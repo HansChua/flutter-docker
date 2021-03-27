0dae6641bdd8cafd723a055ab3d3085a4b8f867e
Hans Chua <h.g.n.chua@gmail.com>
2021-03-28 00:01:34 +0800

    Add trace

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,9 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 echo Flutter version: "$(flutter --version)"
 echo dartcop version: "$(dartcop --version)"
 
-echo "$(<analysis_options.yaml )"
+echo $(ls)
+echo "$(cat analysis_options.yaml)"
+echo dartcop --options analysis_options.yaml .
 
-dartcop --options analysis_options.yaml . \
+edartcop --options analysis_options.yaml . \
   | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -filter-mode="${INPUT_FILTER_MODE}" -level="${INPUT_LEVEL}"
